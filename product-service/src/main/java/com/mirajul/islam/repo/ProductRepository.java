36b6133a4cd5d5e3b901d0245afdd19edc5447d7
mirajul.islam <mirajul.islam@gtechcp.com>
2022-07-17 12:15:00 +0600

    microservice start

--- product-service/src/main/java/com/mirajul/islam/repo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mirajul.islam.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.mirajul.islam.model.Product;
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
