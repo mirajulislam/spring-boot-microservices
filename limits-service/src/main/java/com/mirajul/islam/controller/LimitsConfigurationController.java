2dbbc740c1ff5f39591c1bd53320e26f631dee94
mirajul.islam <mirajul.islam@gtechcp.com>
2022-07-18 13:16:32 +0600

    limits service  to get configuration from application properties

--- limits-service/src/main/java/com/mirajul/islam/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/mirajul/islam/controller/LimitsConfigurationController.java
@@ -1,16 +1,20 @@
 package com.mirajul.islam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mirajul.islam.bean.LimitConfiguration;
+import com.mirajul.islam.dao.Configuration;
 
 @RestController
 public class LimitsConfigurationController {
+	@Autowired 
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
-		LimitConfiguration limitConfiguration = new LimitConfiguration(1000,1);
+		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
 		return limitConfiguration;
 	}
 
