e1732188ee2551cb91629a35ec20a1b8d4241a9d
mirajul.islam <mirajul.islam@gtechcp.com>
2022-07-18 17:43:01 +0600

    Setting up Dynamic Port in the the Response

--- currency-exchange-service/src/main/java/com/mirajul/islam/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/mirajul/islam/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.mirajul.islam.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,15 @@ import com.mirajul.islam.dao.ExchangeValue;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue
-		(@PathVariable String from, @PathVariable String to){
-		
-		return new ExchangeValue(1000l,from,to,BigDecimal.valueOf(65));
+		(@PathVariable String from, @PathVariable String to){	
+		ExchangeValue exchangeValue = new ExchangeValue(1000l,from,to,BigDecimal.valueOf(65));		
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
 	}
 }
