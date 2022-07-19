2514a5d098197b96811be5feac62bb762f6fce81
mirajul.islam <mirajul.islam@gtechcp.com>
2022-07-19 16:49:42 +0600

    add eureka server and register currency-conversion-service in eureka

---
+++ netflix-eureka-naming-server/src/test/java/com/mirajul/islam/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirajul.islam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
