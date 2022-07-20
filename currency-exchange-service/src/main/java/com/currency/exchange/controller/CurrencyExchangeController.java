64ac5076e793a730c0d4dc5c5530129c20b67d94
mirajul.islam <mirajul.islam@gtechcp.com>
2022-07-20 14:05:06 +0600

    spring-cloud-starter-sleuth add for log tracking

--- currency-exchange-service/src/main/java/com/currency/exchange/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/currency/exchange/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.currency.exchange.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,7 @@ import com.currency.exchange.repo.ExchangeValueRepository;
 
 @RestController
 public class CurrencyExchangeController {
-	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private Environment environment;
 	@Autowired
@@ -22,6 +24,7 @@ public class CurrencyExchangeController {
 		(@PathVariable String from, @PathVariable String to){	
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);		
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}", exchangeValue);
 		return exchangeValue;
 	}
 }
