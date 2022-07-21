3aa0a1fecdcff02287ead8154929b66a828622b8
mirajul.islam <mirajul.islam@gtechcp.com>
2022-07-21 13:34:33 +0600

    Enhance limits service - Get configuration from application props - V2

--- limits-service/src/main/java/com/currency/limits/controller/LimitsController.java
+++ limits-service/src/main/java/com/currency/limits/controller/LimitsController.java
@@ -1,16 +1,20 @@
 package com.currency.limits.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.currency.limits.bean.Limits;
+import com.currency.limits.dao.Configuration;
 
 @RestController
 public class LimitsController {
+	@Autowired
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public Limits retrieveLimitsFromConfigurations() {
-		return new Limits(1,100);
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
 	}
 
 }
